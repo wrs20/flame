12b34d3e82134d795e11e3d07ceb1c8f010b33ba
Will Saunders <wrs@wrsaunders.co.uk>
2021-11-14 17:29:50 +0000

    moved to MPI instead of threads as threads were making copies all over the place

--- hello_cpu.jl
+++ hello_cpu.jl
@@ -1,6 +1,7 @@
 using Random
 using Images
-
+using BenchmarkTools
+using MPI
 
 function find_ind(value, N_funcs, d_inc_sum)
     @inbounds begin
@@ -33,15 +34,33 @@ end
 
 
 function average_colours(state_colour, rowx, colx, new_colour)
-    state_colour[rowx, colx, :] = 0.5 * (state_colour[rowx, colx, :] + new_colour)
+    if (rowx == nothing) || (colx == nothing)
+        return
+    end
+    
+    for cx in 1:3
+        @inbounds ctmp = 0.5 * state_colour[rowx, colx, cx]
+        @inbounds ctmp += 0.5 * new_colour[cx]
+        @inbounds state_colour[rowx, colx, cx] = ctmp
+    end
+
 end
 
-function average_thread_colours(global_state_colour, state_colour)
-    N, M, _, N_threads = size(global_state_colour)
-    for threadx in 1:N_threads
-        state_colour[:, :, :] += global_state_colour[:, :, :, threadx]
+
+function reduce_across_ranks(comm, sendbuf)
+    recvbuf = MPI.Reduce(sendbuf, MPI.SUM, 0, comm)
+    return recvbuf
+end
+
+
+function average_across_ranks(comm, sendbuf)
+    recvbuf = reduce_across_ranks(comm, sendbuf)
+    SIZE = MPI.Comm_size(comm)
+    RANK = MPI.Comm_rank(comm)
+    if RANK == 0
+        recvbuf /= SIZE
     end
-    state_colour[:] /= N_threads
+    return recvbuf
 end
 
 
@@ -54,11 +73,14 @@ function point_to_coords(nrow, ncol, x, y)
     y *= 0.5
     
     x = Int(round(x * ncol))
-    x = max(1, x)
-    x = min(ncol, x)
+    if (x<1) || (x>ncol)
+        x = nothing
+    end
+
     y = Int(round(y * nrow))
-    y = max(1, y)
-    y = min(nrow, y)
+    if (y<1) || (y>ncol)
+        y = nothing
+    end
     
     return x, y
 end
@@ -89,13 +111,13 @@ function V_2(x, y)
 end
 
 struct Variation
-    func
-    a
-    b
-    c
-    d
-    e
-    f
+    func::Function
+    a::Float32
+    b::Float32
+    c::Float32
+    d::Float32
+    e::Float32
+    f::Float32
 end
 function(v::Variation)(x, y)
     return v.func(
@@ -104,14 +126,38 @@ function(v::Variation)(x, y)
     )
 end
 
+function run_trajectories(N_traj, N_steps, N_burn_in, nrow, ncol, global_state_freq, global_state_colour, funcs, colours, rng)
+    for trajx in 1:N_traj
+        x, y = new_point(rng)
+        
+        @inbounds begin
+            for stepx in 1:N_steps
+                P = rand(rng)
+                f = find_ind(P, N_funcs, d_inc_sum)
+                x, y = funcs[f](x, y)
+                
+                if stepx > N_burn_in
+                    X, Y = point_to_coords(nrow, ncol, x, y)
+                    average_colours(global_state_colour, Y, X, colours[f])
+                end
+            end
+        end
+    end
+end
 
-funcs = (
-    (V_0,   0.5, [1.0, 0.0, 0.0]),
-    (V_1,   0.5, [0.0, 1.0, 0.0]),
-    (V_2, 0.5, [0.0, 0.0, 1.0]),
-)
 
+MPI.Init()
+COMM = MPI.COMM_WORLD
+RANK = MPI.Comm_rank(COMM)
+SIZE = MPI.Comm_size(COMM)
 
+#funcs = (
+#    (V_0,   0.5, [1.0, 0.0, 0.0]),
+#    (V_1,   0.5, [0.0, 1.0, 0.0]),
+#    (V_2,   0.5, [0.0, 0.0, 1.0]),
+#)
+
+# from https://github.com/scottdraves/flam3/blob/master/test.flam3
 funcs = (
          (Variation(V_2,-0.681206, -0.0779465, 0.20769 ,  0.755065 , -0.0416126, -0.262334), 0.25, [1.0, 0.0, 0.0]),
          (Variation(V_2, 0.953766,  0.48396  , 0.43268 , -0.0542476,  0.642503 , -0.995898), 0.25, [0.0, 1.0, 0.0]),
@@ -119,51 +165,42 @@ funcs = (
          (Variation(V_2, 0.960492, -0.466555 , 0.215383, -0.727377 , -0.126074 ,  0.253509), 0.25, [0.5, 0.5, 0.0]),
 )
 
+if RANK == 0
+    println("SETUP...\n")
+end
 
 weight_sum = sum([fx[2] for fx in funcs])
 d_inc_sum = Array(cumsum([fx[2] / weight_sum for fx in funcs]))
 
-@show d_inc_sum
+nrow = 2000
+ncol = 2000
 
-nrow = 1000
-ncol = 1000
-
-N_traj = 2000
+N_traj = 10000
 N_steps = 10000
-N_burn_in = 100
+N_burn_in = 20
 N_funcs = length(d_inc_sum)
 
-
-N_threads = Threads.nthreads()
-global_state_freq = Array(zeros(Float32, nrow, ncol, N_threads));
-global_state_colour = Array(zeros(Float32, nrow, ncol, 3, N_threads));
-
-state_freq = Array(zeros(Float32, nrow, ncol));
-state_colour = Array(zeros(Float32, nrow, ncol, 3));
+global_state_freq = Array(zeros(Float32, nrow, ncol));
+global_state_colour = Array(zeros(Float32, nrow, ncol, 3));
 
 rng = MersenneTwister(1234);
 
-Threads.@threads for trajx in 1:N_traj
-    
-    thread_id = Threads.threadid()
-    state_freq = view(global_state_freq, :, :, thread_id)
-    state_colour = view(global_state_colour, :, :, :, thread_id)
+call_funcs = [fx[1] for fx in funcs]
+call_colours = [fx[3] for fx in funcs]
 
-    x, y = new_point(rng)
+if RANK == 0
+    println("RUNNING...\n")
+end
+run_trajectories(Int(ceil(N_traj/SIZE)), N_steps, N_burn_in, nrow, ncol, global_state_freq, global_state_colour, call_funcs, call_colours, rng)
 
-    for stepx in 1:N_steps
-        P = rand(rng)
-        f = find_ind(P, N_funcs, d_inc_sum)
-        x, y = funcs[f][1](x, y)
-        if stepx > N_burn_in
-            X, Y = point_to_coords(nrow, ncol, x, y)
-            average_colours(state_colour, Y, X, funcs[f][3])
-        end
-    end
+if RANK == 0
+    println("SAVING...")
 end
 
+reduce_state_colour = average_across_ranks(COMM, global_state_colour)
 
-average_thread_colours(global_state_colour, state_colour)
-save_array_as_image(state_colour, "foo.png")
+if RANK == 0
+    save_array_as_image(reduce_state_colour, "foo.png")
+end
 
-
+MPI.Finalize()
